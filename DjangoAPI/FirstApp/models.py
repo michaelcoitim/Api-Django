23eb2e288abbf5239b53d4edb46b90e646982665
michaelcoitim <michaelcoitim1@gmail.com>
2023-11-17 22:04:00 -0300

    criando camos model

--- DjangoAPI/FirstApp/models.py
+++ DjangoAPI/FirstApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Departaments(models.Model):
+    DepartmentId = models.AutoField(primary_key=True)
+    DepartamentName =models.CharField(max_length=500)
+
+    class Employees(models.Model):
+        EmployeeId = models.AutoField(primary_key=true)
+        Employeename = models.CharField(max_length=500)
+        Department = models.CharField(max_length=500)
+        DateOfJoining = models.DateField()
+        PhotoFileName = models.CharField(max_length=500)
