f2a55191a8dfbedbd46396c05c1f392433e80f11
michaelcoitim <michaelcoitim1@gmail.com>
2023-11-18 17:16:49 -0300

    ultimos ajustes e criação dos endpoins

--- DjangoAPI/FirstApp/models.py
+++ DjangoAPI/FirstApp/models.py
@@ -6,7 +6,7 @@ class Departaments(models.Model):
     DepartmentId = models.AutoField(primary_key=True)
     DepartamentName =models.CharField(max_length=500)
 
-    class Employees(models.Model):
+class Employees(models.Model):
         EmployeeId = models.AutoField(primary_key=True)
         Employeename = models.CharField(max_length=500)
         Department = models.CharField(max_length=500)
